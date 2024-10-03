23d1e8a21fde9a99537aab2029fd4605268d693f
insasautomatic <insasautomatic@gmail.com>
2024-10-03 18:41:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//app.use("/words", require("./routes/words.js"));
+
+app.use("/table/limits", require("./routes/tableLimits.js"));
+app.use("/config", require("./routes/config.js"));
+app.use("/game", require("./routes/game.js"));
+
+app.listen("5000", () => {
+  console.log("Server started on port 5000");
+});
